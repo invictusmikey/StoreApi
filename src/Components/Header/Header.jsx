387700a5a3ae67d5935c66a2b49a1460c2691644
unknown <SenaTarde@ARMAPRCCIFOD657.FormacionSena.RED>
2024-05-08 13:33:21 -0500

    finalizacion del header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Items } from '../Items/Items'
-
+import  icon  from "../../assets/icon.png";
+import searchIcon from "../../assets/searchIcon.png";
 export const Header = () => {
   return (
-    <div>
-        <img src="../assets/icon.png" alt="" />
-        <h1>fakeStoreJhon</h1>
-        <input type="search" name="" id="" />
+
+    <div className='flex  justify-center justify-evenly bg-gray-600 h-20 justify-items-center items-center'>
+      <div className='flex justify-around'>
+        <img src={icon} alt="" className='w-8 h-8' />
+        <h1 className='text-3xl '>fakeStoreJhon</h1>
+      </div>  
+      <div className='flex '>
+       <img src={searchIcon} alt=""  className='w-8 h-8'/>
+        <input  type="search" name="" id="" placeholder='holajhon' className='h-7 w-72 flex justify-center' />
+       
+      </div>
        <Items/>
     </div>
+
   )
 }
--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export const Items = () => {
   return (
-    <div>
-        <ul>
-            <li>inicio</li>
-            <li>yo</li>
-            <li>tienda</li>
+    <div className='w-80 text-3xl'>
+        <ul className='flex  justify-center justify-between text-center'>
+            <li> <a href="#">inicio</a></li>
+            <li><a href="#">yo</a></li>
+            <li><a href="#">tienda</a></li>
         </ul>
     </div>
   )
